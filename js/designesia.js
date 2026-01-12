1ebe9297ebefcefac71a7afae62c74b5c4ee18b7
ABMsiddhik <abmsiddhik@gmail.com>
2026-01-12 11:01:15 +0530

    chillpro name conversion

--- js/designesia.js
+++ js/designesia.js
@@ -1,5 +1,5 @@
  /* --------------------------------------------------
-  * © Copyright 2025 - CoolAir by Designesia
+  * © Copyright 2025 - chillpro
   * --------------------------------------------------*/
 (function($) {
 	'use strict';
